d0cea32cbd711cea9c211f8346faa942f1775e08
tapiwan <herkuleshtc@googlemail.com>
2017-08-10 21:34:09 +0200

    Added index() to HomeController

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -3,9 +3,8 @@
 namespace bitbetrieb\CMS\Controller;
 
 class HomeController extends Controller {
-    public function index($var1, $var2) {
-        echo $var1."<br>";
-        echo $var2."<br>";
+    public function index() {
+
     }
 }
 
