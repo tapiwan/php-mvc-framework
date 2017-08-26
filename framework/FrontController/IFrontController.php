6f9bd5cb56857ded71c7f7ffb6f872405b6ff87a
tapiwan <herkuleshtc@googlemail.com>
2017-08-26 12:26:50 +0200

    Front Controller Interface gekürzt

--- framework/FrontController/IFrontController.php
+++ framework/FrontController/IFrontController.php
@@ -11,17 +11,6 @@ use bitbetrieb\MVC\HTTP\IRequest as IRequest;
  */
 interface IFrontController {
     public function __construct(IRequest $request);
-    public function addExtension($file);
-    public function loadExtension($file);
-    public function loadExtensions();
-    public function addRouteParameter($parameter);
-    public function getRouteParameters();
-    public function setControllerNamespacePrefix($controllerNamespacePrefix);
-    public function getControllerNamespacePrefix();
-    public function get($route, $callable);
-    public function post($route, $callable);
-    public function put($route, $callable);
-    public function delete($route, $callable);
     public function addRoute($method, $route, $callable);
     public function setErrorHandler($callable);
     public function execute();
